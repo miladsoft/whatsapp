f2b456e928a1082e79f09048d850f6087d958aa0
Milad Raeisi <miladsoft@yahoo.com>
2025-02-05 12:27:39 +0400

    Upload App

---
+++ BaileysCSharp/Core/Helper/ProcessingMutex.cs
@@ -0,0 +1,24 @@
+﻿namespace BaileysCSharp.Core.Helper
+{
+    public class ProcessingMutex
+    {
+        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+        public ProcessingMutex()
+        {
+
+        }
+
+        public async Task Mutex(Action action)
+        {
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+            }
+            semaphoreSlim.Release();
+        }
+    }
+}
